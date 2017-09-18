7371c5868653617d2390596c9511438ad35f722d
Avinesh Kumar <avinesh.in@gmail.com>
2017-09-18 21:20:13 +0530

    Update HttpServerImpl.py

--- HttpServerImpl.py
+++ HttpServerImpl.py
@@ -1,12 +1,13 @@
 from SimplePyHttpServer import MyHandler
 import time
 import subprocess
+import json
 
 # you can implement here from simple to complex logic in order to get the work done.
 # here i have implemented ifconfig command.
 # http://localhost/?command=ifconfig ==> this request to server will return ifconfig command output on host.
 
-def perform_command(query_components):
+def process_get(query_components):
     command = query_components.get('command')
     cmd = command[0]
     if cmd == 'ifconfig':
@@ -14,3 +15,13 @@ def perform_command(query_components):
         return nw_config
     else:
         return "invalid command."
+
+
+def process_post(data):
+    eid = data["eid"]
+    processed = {}
+    processed["name"] = "Avinesh Kumar"
+    processed["id"] = eid
+    processed["email"]= "avinesh@gmail.com"
+    json_data = json.dumps(processed)
+    return json_data
