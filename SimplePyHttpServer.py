99910e065c4ab68c3cecc24cbe095dedf8ed78e4
Avinesh Kumar <avinesh.in@gmail.com>
2017-09-19 20:03:45 +0530

    SimplePyHttpServer

--- SimplePyHttpServer.py
+++ SimplePyHttpServer.py
@@ -4,13 +4,15 @@ import BaseHTTPServer
 import time
 import HttpServerImpl
 import json
+from urlparse import urlparse, parse_qs
 
 hostname="127.0.0.1"
 port=80
 
+
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    counter = 0
-    
+    keep_values = {}
+
     def do_HEAD(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
@@ -20,17 +22,18 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        from urlparse import urlparse, parse_qs
+        # print self.path
+        # print urlparse(self.path).query
         query_components = parse_qs(urlparse(self.path).query)
+        # print query_components
         res = HttpServerImpl.process_get(query_components)
-        print "response: ",res
+        # print "response: ",res
         self.wfile.write(res)
-
 
     def do_POST(self):
         content_len = int(self.headers.getheader('content-length'))
         post_body = self.rfile.read(content_len)
-        print "post body: ",post_body
+        # print "post body: ",post_body
         self.send_response(200)
         self.end_headers()
         data = json.loads(post_body)
